8afba73ce7c49787fcbb787ee5161bda3a0ce085
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-05-28 11:39:20 +0200

    51

--- src/config.js
+++ src/config.js
@@ -1,8 +1,4 @@
-// 📁 config.js
-
-export const SUPABASE_PROJECT_URL = typeof window !== 'undefined' && window?.ENV?.VITE_SUPABASE_PROJECT_URL
-  ? window.ENV.VITE_SUPABASE_PROJECT_URL
-  : "https://edzvwddbrdokwutmxfdx.supabase.co";
+export const SUPABASE_PROJECT_URL = "https://edzvwddbrdokwutmxfdx.supabase.co";
 export const SUPABASE_API_KEY = process.env.REACT_APP_SUPABASE_API_KEY;
 export const SUPABASE_BUCKET = "plannerdata";
 
