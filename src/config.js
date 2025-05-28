eabc36ec5eb81acda63c916d642db5a9fce3aa35
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-05-28 10:19:39 +0200

    43

--- src/config.js
+++ src/config.js
@@ -7,4 +7,4 @@ export const SUPABASE_API_KEY = process.env.REACT_APP_SUPABASE_API_KEY;
 export const SUPABASE_BUCKET = "plannerdata";
 
 export const SUPABASE_STORAGE_URL = `${SUPABASE_PROJECT_URL}/storage/v1/object`;
-export const SUPABASE_PUBLIC_BASE = `${SUPABASE_STORAGE_URL}/public/${SUPABASE_BUCKET}`;
+export const SUPABASE_PUBLIC_BASE = `${SUPABASE_STORAGE_URL}/${SUPABASE_BUCKET}`;
