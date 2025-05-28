626bf1b6fbdd5cc1d17d759c5139e1203ec8fd6b
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-05-28 19:20:39 +0200

    74

--- src/PlannerBoard.jsx
+++ src/PlannerBoard.jsx
@@ -289,7 +289,7 @@ export default function PlannerBoard({ beschikbaarheid: beschikbaarheidProp, pla
         </table>
       )}
 
-      {popup && (
+            {popup && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white p-4 rounded shadow-md w-[400px]">
             <h2 className="font-bold mb-4 text-center">
@@ -317,3 +317,6 @@ export default function PlannerBoard({ beschikbaarheid: beschikbaarheidProp, pla
         </div>
       )}
 
+    </div> {/* sluit de <div className="p-4 bg-gray-100"> */}
+  );
+}
