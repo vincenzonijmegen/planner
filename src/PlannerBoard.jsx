f02b1e225ad18bba961d3ec6b0602bc08205199e
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-05-29 20:15:28 +0200

    ✨ MaxShifts & opmerkingen uit Excel toegevoegd

--- src/PlannerBoard.jsx
+++ src/PlannerBoard.jsx
@@ -102,8 +102,6 @@ export default function PlannerBoard({ beschikbaarheid: beschikbaarheidProp }) {
 
     setMedewerkers(medewerkersMetKleur);
   }, [planning]);
-
-  
 }, [localBeschikbaarheid]);
 
 
